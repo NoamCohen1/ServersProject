0fbaf79fa1f03019d8db717a7fed262ef56d28ce
Noam Cohen <noamhilac@gmail.com>
2019-01-07 11:54:04 +0200

    open Socket and thread

--- MyTestClientHandler.cpp
+++ MyTestClientHandler.cpp
@@ -14,23 +14,29 @@ void MyTestClientHandler::solveProblem(int sockfd) {
         perror("ERROR on accept");
         exit(1);
     }
-//
-    /* If connection is established then start communicating */
-    bzero(buffer,256);
-    n = read( sockfd,buffer,255 );
 
-    if (n < 0) {
-        perror("ERROR reading from socket");
-        exit(1);
-    }
+    while (true) {
+        /* If connection is established then start communicating */
+        bzero(buffer, 256);
+        n = read(sockfd, buffer, 255);
 
-    printf("Here is the message: %s\n",buffer);
+        if (n < 0) {
+            perror("ERROR reading from socket");
+            exit(1);
+        }
 
-    /* Write a response to the client */
-    n = write(sockfd,"I got your message",18);
+        if (buffer == "end") {
+            return;
+        }
 
-    if (n < 0) {
-        perror("ERROR writing to socket");
-        exit(1);
+        printf("Here is the message: %s\n", buffer);
+
+        /* Write a response to the client */
+        n = write(sockfd, "I got your message", 18);
+
+        if (n < 0) {
+            perror("ERROR writing to socket");
+            exit(1);
+        }
     }
 }
